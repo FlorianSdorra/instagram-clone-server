e158787798c89ea9541b7f0b1f0475d0c623d39c
FlorianSdorra <fsd114@posteo.de>
2020-11-10 20:58:36 +0100

    added password encryption in User signup

--- routes/User.js
+++ routes/User.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
 import mongoose from 'mongoose';
-const User = mongoose.model("User");
+import User from '../models/user.js';
+import bcrypt from 'bcryptjs';
 
 router.get('/', (req, res)=>{
     res.send('hello')
@@ -17,22 +18,25 @@ router.post('/signup', (req, res)=>{
         if(savedUser){
             return res.status(422).json({error:"user already exists with this email"})
         }
-        const user = new User({
-            email,
-            password,
-            name
+        bcrypt.hash(password, 12)
+        .then(hashedpassword=>{
+            const user = new User({
+                email,
+                password:hashedpassword,
+                name
+            })
+    
+            user.save()
+                .then(user=>{
+                    res.json({message: "You signed up succesfully"})
+                })
+                .catch(err=>{
+                    console.log(err)
+                })
         })
-
-        user.save()
-            .then(user=>{
-                res.json({message: "saved succesfully"})
-            })
-            .catch(err=>{
-                console.log(err)
-            })
-    })
-    .catch(err=>{
-        console.log(err)
+        .catch(err=>{
+            console.log(err)
+        })
     })
 })
 
